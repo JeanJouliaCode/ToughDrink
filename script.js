ebfa72d33c98ada9674192f18d10ecc6d4dac0a3
jean joulia <jean.joulia@niji.fr>
2020-09-15 15:08:41 +0200

    glass and water rise

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function startGame() {
       console.log('Im alive');
+
+      waterAndGlassUp();
+}
+
+
+
+function waterAndGlassUp() {
+      var water = document.getElementById("water");
+      var glass = document.getElementById("glass");
+
+      water.classList.add('waterUp');
+      glass.classList.add('glassUp');
 }
